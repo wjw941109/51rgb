ceb83437e4bc3182512d435ba9c922ea2b120274
wjw941109 <2459604359@qq.com>
2019-02-18 21:36:52 +0800

    commit diansahng

--- dianshang/js/main.js
+++ dianshang/js/main.js
@@ -23,4 +23,24 @@ $(function () {
 		$(this).children().children().children('.hover-show').css('display','block').siblings().addClass('hide');
     	$(this).siblings().children().children().children('.hover-show').css('display','none').siblings().removeClass('hide');
     });
+
+
+
+    $(".focusbox ul li").click(function(){
+      var index = $(this).index();
+      $(this).addClass("on").siblings().removeClass("on");
+      $(".focusbox .banner img").hide().eq(index).show();
+    });
+    setInterval(function(){
+      var li = $(".focusbox ul li");
+      var li_length = li.length;
+      var li_on = $(".focusbox ul li.on");
+      var on_index = li.index(li_on);
+      if (on_index+1<li_length) {
+        $(".focusbox li.on").next().click();
+      }else{
+        li.eq(0).click();
+      }
+
+    },2000)
 })
